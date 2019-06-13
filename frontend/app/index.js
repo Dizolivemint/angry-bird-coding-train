ef5f08139538c2ffea9cd0d8415a9f0cd7076f90
Miles Exner <dizolivemint@gmail.com>
2019-06-12 17:49:26 -0700

    Initial import

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,26 +1,6 @@
-let img;
-let button;
-
-function setup() {
-  // make a full screen canvas
-  createCanvas(window.innerWidth, window.innerHeight);
-  img = loadImage(Koji.config.images.mouse); // Load the image
-
-}
-
-function draw() {
-  // set the background color from the configuration options
-  background(Koji.config.colors.backgroundColor);
-
-  // format our text
-  textSize(24);
-  fill(Koji.config.colors.textColor);
-  textAlign(CENTER);
-
-  // print out our text
-  text(Koji.config.strings.content, window.innerWidth / 2, 100);
-
-  // setup an image to follow our mouse
-  let imageSize = 100;
-  image(img, mouseX - (imageSize / 2), mouseY - (imageSize / 2), imageSize, imageSize);
-}
+import matter from 'matter-js'
+import slingshot from './game/slingshot'
+import bird from './game/bird'
+import box from './game/box'
+import ground from './game/ground'
+import sketch from './game/sketch'
